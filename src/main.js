c0aac15ac7ba00783971460f4ff0322d3b22128c
gustavofsa <gugafarias2009@gmail.com>
2020-05-05 16:13:48 -0300

    class App and render feature

--- src/main.js
+++ src/main.js
@@ -1,1 +1,57 @@
-alert('teste');
+class App {
+  constructor() {
+    this.repositories = [];
+
+    this.formEl = document.getElementById('repo-form');
+    this.listEl = document.getElementById('repo-list');
+
+    this.registerHandlers();
+  }
+
+  registerHandlers() {
+    this.formEl.onsubmit = event => this.addRepository(event);
+  }
+
+  addRepository(event) {
+    event.preventDefault();
+
+    this.repositories.push({
+      name: 'rocketseat.com.br',
+      description: 'Tire sua ideia do papel e dê vida à sua startup.',
+      avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
+      html_url: 'https://github.com/rocketseat/rocketseat.com.br',
+    });
+
+    this.render();
+  }
+
+  render() {
+    this.listEl.innerHTML = '';
+
+    this.repositories.forEach(repo => {
+      let imgEl = document.createElement('img');
+      imgEl.setAttribute('src', repo.avatar_url);
+
+      let titleEl = document.createElement('strong');
+      titleEl.appendChild(document.createTextNode(repo.name));
+
+      let descriptionEl = document.createElement('p');
+      descriptionEl.appendChild(document.createTextNode(repo.description));
+
+      let linkEl = document.createElement('a');
+      linkEl.setAttribute('href', repo.html_url);
+      linkEl.setAttribute('target', '_blank');
+      linkEl.appendChild(document.createTextNode('Acessar'));
+
+      let listItemEl = document.createElement('li');
+      listItemEl.appendChild(imgEl);
+      listItemEl.appendChild(titleEl);
+      listItemEl.appendChild(descriptionEl);
+      listItemEl.appendChild(linkEl);
+
+      this.listEl.appendChild(listItemEl);
+    });
+  }
+}
+
+new App();
